fd874c1cc6c34f5e8421f214d5f1116ccf15ec85
Shahriar <shahriar3809@gmail.com>
2024-03-25 20:02:49 +0600

    first commit

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+
+
+const Root = () => {
+    return (
+        <div className="max-w-6xl mx-auto">
+            This is root
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
